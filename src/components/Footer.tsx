54f2079447bb831ab13740dfe6ff242b76e4bd82
jose <joseluis_1555@hotmail.com>
2025-07-04 09:17:58 -0500

    FIX: Corregir footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,44 +3,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-8 px-4">
-      <div className="max-w-6xl mx-auto">
-        {/* <div className="grid md:grid-cols-4 gap-8">
-          <div className="col-span-2">
-              <img src="/logo_blanco.png" className='w-36 h-36 pb-10' />
-
-            <p className="text-gray-300 mb-4 max-w-md">
-              La plataforma de e-commerce más segura y confiable del Perú. 
-              Conectamos compradores y vendedores con garantías reales.
-            </p>
-            <div className="flex space-x-4">
-              <div className="bg-red-600 p-2 rounded-full">
-                <span className="text-white font-bold">P</span>
-              </div>
-            </div>
-          </div>
-          
-          <div>
-            <h4 className="text-lg font-semibold mb-4">Características</h4>
-            <ul className="space-y-2 text-gray-300">
-              <li>Verificación de vendedores</li>
-              <li>Seguimiento completo</li>
-              <li>Garantías totales</li>
-              <li>Envíos rápidos</li>
-            </ul>
-          </div>
-          <div>
-            <h4 className="text-lg font-semibold mb-4">Soporte</h4>
-            <ul className="space-y-2 text-gray-300">
-              <li>Centro de ayuda</li>
-              <li>Política de devoluciones</li>
-              <li>Términos y condiciones</li>
-              <li>Contacto</li>
-            </ul>
-          </div>
-        </div> */}
-        
-        <div className=" border-gray-800 mt-0 pt-0 text-center">
+    <footer className="bg-gray-900  py-8 px-4">
+      <div className="max-w-6xl mx-auto grid grid-cols-2">
+        <p className='text-gray-300'>
+          Contacto:<br/>
+          -info@lapitayastore.com,<br/>-atencionalcliente@lapitayastore.com
+        </p>
+        <div className=" border-gray-800 mt-0 pt-0 text-center self-center">
           <p className="text-gray-400">
             © 2024 Pitaya. Todos los derechos reservados. 
           </p>
